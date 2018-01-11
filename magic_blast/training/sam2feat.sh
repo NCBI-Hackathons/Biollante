da285d860f626242587f2de8cb595bccc67f44c5
Adrian Cantu <garbanyo@gmail.com>
2018-01-11 17:54:21 -0500

    fixed order of paste

--- magic_blast/training/sam2feat.sh
+++ magic_blast/training/sam2feat.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 grep -v '^@' $1 | cut -f2 -s  | head | xargs -I{} sh -c 'echo "obase=2;{}" | bc' | xargs printf '%012d\n' | rev | perl -ne  'chomp; print join("\t", split(//, $_)), "\n"' > tmp1
-grep -v '^@' $1 | cut -f1,3,5,9 > tmp2
-paste tmp1 tmp2 > ${1%.*}_table.tsb
-rm tmp1 tmp2
+grep -v '^@' $1 | cut -f3,5 > tmp2
+paste tmp2 tmp1 > ${1%.*}_table.tsb
+
+
+#rm tmp1 tmp2
