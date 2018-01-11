bb534112fdc1b72119e41e0084555413308d4af6
Adrian Cantu <garbanyo@gmail.com>
2018-01-11 14:34:50 -0500

    Added csc -> fasta script

--- magic_blast/run_magicblast.sh
+++ magic_blast/run_magicblast.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-cut -f 1 SRA_codes.txt -d' ' | xargs -I{} echo 'magicblast -sra {} -db ~/data/DB/PlantDB -num_threads 6 -splice F -out {}_vs_PlantDB.sam' 
+cut -f 1 SRA_codes.txt -d' ' | xargs -I{} echo 'magicblast -sra {} -db ~/data/DB/PlantDB -num_threads 6 -splice F | gzip -c > {}_vs_PlantDB.sam.gz' 
