4b333869dec6d8a6cb7e447f6c4d41d974be9760
Adrian Cantu <garbanyo@gmail.com>
2018-01-11 19:23:54 -0500

    change plant database

--- magic_blast/training/sam2feat.sh
+++ magic_blast/training/sam2feat.sh
@@ -2,7 +2,7 @@
 
 grep -v '^@' $1 | cut -f2 -s  | head | xargs -I{} sh -c 'echo "obase=2;{}" | bc' | xargs printf '%012d\n' | rev | perl -ne  'chomp; print join("\t", split(//, $_)), "\n"' > tmp1
 grep -v '^@' $1 | cut -f3,5 > tmp2
-paste tmp2 tmp1 > ${1%.*}_table.tsb
+paste tmp2 tmp1 > ${1%.*}_table.tsv
 
 
-#rm tmp1 tmp2
+rm tmp1 tmp2
