e4469de243291c8eb863d45bbc6d15a575870302
Shashwat Kishore <kishore@college.harvard.edu>
2018-01-12 11:44:23 -0800

    added pickle dump

--- initial_pipeline.py
+++ initial_pipeline.py
@@ -121,6 +121,8 @@ predictions = [round(value) for value in y_pred]
 accuracy = accuracy_score(y_test, predictions)
 print("Accuracy: %.2f%%" % (accuracy * 100.0))
 
+with open('model', 'wb') as h:
+    pickle.dump(model, h)
 
 #with open('resistance_feature_vectors_2.p', 'wb') as h:
 
